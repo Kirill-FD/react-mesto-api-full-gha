113a0f3694b1dc26d5b3af30e725c38d3e625c72
Kirill Polischuk <120019115+Kirbro36@users.noreply.github.com>
2023-10-11 22:38:02 +0300

    Update users.js

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -7,7 +7,7 @@ const BadRequestError = require('../errors/BadRequestError');
 const NotFoundError = require('../errors/NotFoundError');
 const ConflictError = require('../errors/ConflictError');
 
-const { SECRET_KEY } = process.env;
+const { SECRET_KEY = 'mesto' } = process.env;
 
 module.exports.getUsers = (req, res, next) => {
   User.find({})
