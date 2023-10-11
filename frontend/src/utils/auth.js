222050b75fedc2a897186c312b641cd523424c9f
Kirill Polischuk <120019115+Kirbro36@users.noreply.github.com>
2023-10-12 00:47:12 +0300

    Update auth.js

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-const baseUrl = 'api.kirbro69.nomoredomainsrocks.ru'
+const baseUrl = 'https://api.kirbro69.nomoredomainsrocks.ru'
 
 function getResData(res) {
   return res.ok ? res.json() : Promise.reject(`${res.status} ${res.statusText}`)
