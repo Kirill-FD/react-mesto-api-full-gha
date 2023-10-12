b096eeba11721232e9b2359bd861e2139af52ae4
Kirill Polischuk <120019115+Kirbro36@users.noreply.github.com>
2023-10-12 10:26:44 +0300

    Update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -21,8 +21,8 @@ const userSchema = new mongoose.Schema({
     type: String,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     validate: {
-      validator(v) {
-        return urlRegex.test(v);
+      validator(url) {
+        return urlRegex.test(url);
       },
       message: 'Невалидный URL',
     },
